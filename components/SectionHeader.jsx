8f4c121b9287df227f4808fb1b3a3541f5a99780
Ben Langlois <bdlanglo@edu.uwaterloo.ca>
2019-09-08 23:07:10 -0400

    Create SectionHeader

---
+++ components/SectionHeader.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+  background-color: black;
+  position: relative;
+  height: 7rem;
+  margin-bottom: 5rem;
+  letter-spacing: 8px;
+`;
+
+const HeaderText = styled.div`
+  font-size: 80px;
+  color: white;
+  font-family: Helvetica, Tahoma, Geneva, sans-serif;
+  font-weight: bold;
+  left: 15%;
+  bottom: -18px;
+  position: absolute;
+`;
+
+/**
+ * The header for a section of the page
+ * @param {{ children: React.ReactNode }} props
+ */
+export default props => {
+  return (
+    <HeaderContainer>
+      <HeaderText>{props.children}</HeaderText>
+    </HeaderContainer>
+  );
+};
