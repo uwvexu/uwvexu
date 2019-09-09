131e998096938c72cb9d17013547a4ca36252b5d
Ben Langlois <bdlanglo@edu.uwaterloo.ca>
2019-09-08 23:07:17 -0400

    Create SectionBody

---
+++ components/SectionBody.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: relative;
+  left: 15%;
+  width: 70%;
+`;
+
+/**
+ * The body container of a section
+ * @param {{ children: React.ReactNode }} props
+ */
+export default props => {
+  return <Container>{props.children}</Container>;
+};
