219f46d81330d495a27742dcc78d3006fabfb4ca
Alexandra Girard <alexgirard999@gmail.com>
2019-09-11 23:07:50 -0400

    test scss

--- next.config.js
+++ next.config.js
@@ -5,17 +5,10 @@ const debug = process.env.NODE_ENV !== "production";
 
 const nextConfig = {
   assetPrefix: !debug ? '/uwvexu.ca/' : '',
-  webpack: (config, { dev }) => {
-    // Perform customizations to webpack config
-    config.module.rules = config.module.rules.map(rule => {
-      if(rule.loader === 'babel-loader') {
-        rule.options.cacheDirectory = false
-      }
-      return rule
-    })
-    // Important: return the modified config
-    return config
-  }
 };
 
-module.exports = withSass(nextConfig);
+module.exports = withSass({
+  webpack(config, options) {
+    return config
+  }
+});
