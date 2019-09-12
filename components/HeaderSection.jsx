241e8f2f50586571a7af5d1799e0b3660fee1bdb
Ben Langlois <bdlanglo@edu.uwaterloo.ca>
2019-09-11 20:51:04 -0400

    Make section header responsive

--- components/HeaderSection.jsx
+++ components/HeaderSection.jsx
@@ -1,20 +1,62 @@
 import React from 'react';
 import styled from 'styled-components';
+import { responsiveSizes } from './Styles';
 
 const HeaderContainer = styled.div`
   background-color: black;
   position: relative;
   height: 7rem;
-  letter-spacing: 8px;
+
+  @media screen and (max-width: ${responsiveSizes.smallScreen}px) {
+    height: 4.5rem;
+  }
+
+  @media screen and (max-width: ${responsiveSizes.tablet}px) {
+    height: 3rem;
+  }
+
+  @media screen and (max-width: ${responsiveSizes.largeMobile}px) {
+    height: 2.25rem;
+  }
 `;
 
 const HeaderText = styled.div`
   font-size: 80px;
   color: white;
   font-weight: bold;
-  margin-left: 15rem;
+  margin: 0 16rem;
   bottom: -18px;
   position: absolute;
+  letter-spacing: 8px;
+
+  @media screen and (max-width: ${responsiveSizes.largeScreen}px) {
+    margin: 0 12rem;
+  }
+
+  @media screen and (max-width: ${responsiveSizes.smallScreen}px) {
+    margin: 0 8rem;
+    font-size: 50px;
+    letter-spacing: 6px;
+    bottom: -12px;
+  }
+
+  @media screen and (max-width: ${responsiveSizes.tablet}px) {
+    margin: 0 6rem;
+    font-size: 28px;
+    letter-spacing: 4px;
+    bottom: -6px;
+  }
+
+  @media screen and (max-width: ${responsiveSizes.largeMobile}px) {
+    margin: 0 4rem;
+    font-size: 24px;
+    letter-spacing: 2px;
+    bottom: -6px;
+  }
+
+  @media screen and (max-width: ${responsiveSizes.smallMobile}px) {
+    margin: 0 2rem;
+  }
 `;
 
 /**
