c59d65f08a68d50f6813234f9da49760ca40e19a
Alexandra Girard <alexgirard999@gmail.com>
2019-09-11 22:16:08 -0400

    add env back in

---
+++ .babelrc.js
@@ -0,0 +1,16 @@
+const env = require('./env-config')
+
+module.exports = {
+  presets: [
+    'next/babel'
+  ],
+  plugins: [
+    ['transform-define', env],
+    ['babel-plugin-styled-components', {
+        ssr: true,
+        displayName: true,
+        preprocess: false
+      }
+    ]
+  ]
+}
