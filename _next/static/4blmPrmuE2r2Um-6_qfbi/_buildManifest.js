a7465cb5f51519a1d7a194a5db0f0fbbf1ad9426
Alexandra Girard <alexgirard999@gmail.com>
2021-06-11 19:24:34 -0400

    Deploy Next.js to gh-pages

---
+++ _next/static/4blmPrmuE2r2Um-6_qfbi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/97-2195966a7f066ded5c9c.js","static/chunks/pages/index-008b59d9e59cba9914e1.js"],"/_error":["static/chunks/pages/_error-ad2dc25fba989575dd0f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
